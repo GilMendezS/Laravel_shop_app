23bcf767276a500d7e8f4c5e6669501d9316027b
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-12-21 16:14:43 -0600

    :bookmark: status model - client

---
+++ resources/js/models/status.js
@@ -0,0 +1,9 @@
+export default class Status {
+    constructor(id, title, description, created_at, updated_at){
+        this.id = id;
+        this.title = title,
+        this.description = description;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+}
