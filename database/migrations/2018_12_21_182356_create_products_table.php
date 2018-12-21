7c2aad7aa7041306abb2290f05b25abd6ccd9bc2
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-12-21 12:44:17 -0600

    :bookmark: migratio to create products table

---
+++ database/migrations/2018_12_21_182356_create_products_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->softDeletes();
+        });
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->boolean('featured')->default(FALSE);
+            $table->integer('price');
+            $table->boolean('inlimited')->default(FALSE);
+            $table->integer('stock')->default(1);
+            $table->string('number')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('barcode')->nullable();
+            $table->integer('status_id')->unsigned();
+            $table->integer('weight')->nullable();
+            $table->foreign('status_id')->references('id')->on('statuses');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+        Schema::create('images', function(BluePrint $table){
+            $table->increments('id');
+            $table->string('url');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+        Schema::dropIfExists('images');
+        Schema::dropIfExists('products');
+    }
+}
